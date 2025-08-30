60d90de9e54742a9c2559db0973be755e7ed660a
eriky21-21 <eriky21@gmail.com>
2025-08-30 15:42:15 -0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -10,7 +10,7 @@ const nextConfig = {
   typescript: {
     ignoreBuildErrors: true,
   },
-  output: 'standalone',
+  
   compress: true,
   poweredByHeader: false,
   // Adicione esta configuração para Railway
